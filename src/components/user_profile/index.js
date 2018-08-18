000db09775a7dbd13943ee16298b03c4867db7e7
Thulani <thulani@blacklight.co.za>
2018-08-19 00:20:50 +0200

    initialized new branch

---
+++ src/components/user_profile/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div className="user-profile">
+            <div className="user-avatar">
+            <div className="user-details">
+            </div>
+            </div>
+        </div> );
+    }
+}
+ 
+export default UserProfile;
